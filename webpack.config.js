110725c54f5cff93636ea010a1e3a7959c9eb605
Aagii <agii_0930g@yahoo.com>
2024-04-09 14:58:30 +0900

    first message

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/js/index.js",
+  },
+
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  optimization: {
+    runtimeChunk: "single",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
